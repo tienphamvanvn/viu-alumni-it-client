8039e7b38540ad1a2fdaffb53d7e1f42cb732d97
Tien Pham <tienphamvanvn@gmail.com>
2021-10-03 22:30:02 +0700

    change loader

--- src/app/components/loader/loader.componnet.tsx
+++ src/app/components/loader/loader.componnet.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { ReactComponent as IconLoading } from "@/app/assets/svgs/icon-loading.svg";
+import { Spinner } from "@vechaiui/spinner";
 
 const Loader = () => {
   return (
     <div className="h-full w-full flex justify-center items-center absolute z-50 inset-0 bg-white">
-      <IconLoading className="animate-spin h-10 w-10 text-blue-600" />
+      <Spinner size="lg" className="text-blue-600" />
     </div>
   );
 };
